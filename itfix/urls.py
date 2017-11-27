715ffd85b1f76735480a4b8d891caf11f2f5c568
Ignas Grigaravicius <kickeris@gmail.com>
2017-11-27 08:43:17 +0000

    products html updated, base html upadated with bootstarp tags to inport other page content, media root inported in urls.py, static folder created for static images

--- itfix/urls.py
+++ itfix/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from home.views import get_index
 from products import urls as products_urls
 from django.views import static
+from .settings import MEDIA_ROOT
 from rest_framework import routers
 from products import views as product_views
 
@@ -14,5 +15,6 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
     url(r'^products/', include(products_urls)),
-    url(r'^$', get_index, name='index')
+    url(r'^$', get_index, name='index'),
+    url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
 ]
