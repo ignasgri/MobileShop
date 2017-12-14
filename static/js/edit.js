05004577939341f8b92ce4ae590c902cb42bb9f1
Ignas Grigaravicius <kickeris@gmail.com>
2017-12-14 23:54:11 +0000

    products html edited, categories css edited(.card), edit.js added code to scrole to top, scole to top button added in base html

--- static/js/edit.js
+++ static/js/edit.js
@@ -3,3 +3,33 @@ $(document).ready(function(){
         $(this).next(".panel").slideToggle("slow");
     });
 });
+
+// link scroller
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(event) {
+        event.preventDefault();
+  
+        var target = this.hash;
+        var $target = $(target);
+  
+        $('html, body').animate({
+          'scrollTop': $target.offset().top
+        }, 1000, 'swing')
+          window.location.hash = target;
+        });
+    });
+  // to top scroller
+  $(document).ready(function(){
+        $('body').append('<div id="toTop"</div>');
+          $(window).scroll(function () {
+              if ($(this).scrollTop() != 0) {
+                  $('#toTop').fadeIn();
+              } else {
+                  $('#toTop').fadeOut();
+              }
+          }); 
+      $('#toTop').click(function(){
+          $("html, body").animate({ scrollTop: 0 }, 600);
+          return false;
+      });
+  });
