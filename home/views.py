6b3d62e8fec7b988dd94155f2ae39ae273063479
Ignas Grigaravicius <kickeris@gmail.com>
2017-12-24 00:15:55 +0000

    home views py updated to filter products by latest date and pagination update, root urls updated

--- home/views.py
+++ home/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render, get_list_or_404
 from products.models import Product
 from rest_framework import viewsets
+from django.utils import timezone
 from products.serializers import ProductSerializer
 from django.template.context_processors import csrf
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def get_index(request):
-    products = Product.objects.all()
-    # return render(request, 'index.html')
-    return render(request, 'index.html', {"products": products})
+# def get_index(request):
+#     products = Product.objects.all()
+#     # return render(request, 'index.html')
+#     return render(request, 'index.html', {"products": products})
 
 # Create your views here.
-def get_index(request):
-    products = Product.objects.all()
-    paginator = Paginator(products, 6)
+# def get_index(request):
+#     products = Product.objects.all()
+#     paginator = Paginator(products, 6)
+#     page = request.GET.get('page')
+#     try:
+#         products = paginator.page(page)
+#     except PageNotAnInteger:
+#         products = paginator.page(1)
+#     except EmptyPage:
+#         products = paginator.page(paginator.num_pages)
+#     args = {}
+#     args.update(csrf(request))
+#     return render(request, "index.html", {"products": products}, args)
+
+# class ProductViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+def latest_products(request):
+    products = Product.objects.filter(published_date__lte=timezone.now()
+        ).order_by('-published_date')[0:12]
+    paginator = Paginator(products, 3)
     page = request.GET.get('page')
     try:
         products = paginator.page(page)
@@ -23,14 +46,10 @@ def get_index(request):
         products = paginator.page(paginator.num_pages)
     args = {}
     args.update(csrf(request))
-    return render(request, "index.html", {"products": products}, args)
-
-
-
-
+    return render(request, "index.html", {'products': products})
 class ProductViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+#     """
+#     API endpoint that allows users to be viewed or edited.
+#     """
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
--- itfix/urls.py
+++ itfix/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from home.views import get_index
+from home.views import latest_products
 from products import urls as products_urls
 from categories import urls as categories_urls
 from django.views import static
@@ -8,6 +8,7 @@ from .settings import MEDIA_ROOT
 from rest_framework import routers
 from products import views as product_views
 from search import urls as search_urls
+
 
 router = routers.DefaultRouter()
 router.register(r'products', product_views.ProductViewSet)
@@ -18,7 +19,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^products/', include(products_urls)),
     url(r'^categories/', include(categories_urls)),
-    url(r'^$', get_index, name='index'),
+    # url(r'^$', get_index, name='index'),
+    url(r'^$', latest_products, name='index'),
     url(r'^media/(?P<path>.*)$', static.serve,{'document_root': MEDIA_ROOT}),
     url(r'^search/', include(search_urls)),
+    
 ]
